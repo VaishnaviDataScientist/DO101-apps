1571231ac60dc7d1f0c1aa2ec0fd0c5d089504b5
VaishnaviDataScientist <vaishnavi.data.prof@gmail.com>
2024-10-12 17:35:21 -0400

    updated to 3rd verison

--- version/app.js
+++ version/app.js
@@ -11,10 +11,20 @@ app = express();
 //     res.send(response);
 
 // });
+// var response;
+
+// app.get('/', function (req, res) {
+//     response = 'This is version 2 of the app.' + '\n';
+
+//     //send the response to the client
+//     res.send(response);
+
+// });
+
 var response;
 
 app.get('/', function (req, res) {
-    response = 'This is version 2 of the app.' + '\n';
+    response = 'This is version 3 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
